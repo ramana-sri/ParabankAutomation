8cff568cfa1bc7b27287cf76e9184808cf45e818
unknown <ramana.sri@gmail.com>
2025-06-28 00:47:56 +0530

    commented global-teardown.js to run scripts from jenkins

--- global-teardown.js
+++ global-teardown.js
@@ -1,15 +1,15 @@
- const { execSync } = require('child_process');
+//  const { execSync } = require('child_process');
 
-    async function globalTeardown() {
-      // Generate Allure report
-      console.log('Generating Allure report...');
-      try {
-        execSync('npx allure generate allure-results --clean -o allure-report', { stdio: 'inherit' });
-        console.log('Allure report generated successfully in allure-report folder.');
-        execSync('npx allure open allure-report', { stdio: 'inherit' });
-      } catch (error) {
-        console.error('Error generating Allure report:', error.message);
-      }
-    }
+//     async function globalTeardown() {
+//       // Generate Allure report
+//       console.log('Generating Allure report...');
+//       try {
+//         execSync('npx allure generate allure-results --clean -o allure-report', { stdio: 'inherit' });
+//         console.log('Allure report generated successfully in allure-report folder.');
+//         execSync('npx allure open allure-report', { stdio: 'inherit' });
+//       } catch (error) {
+//         console.error('Error generating Allure report:', error.message);
+//       }
+//     }
 
-    module.exports = globalTeardown;
+//     module.exports = globalTeardown;
